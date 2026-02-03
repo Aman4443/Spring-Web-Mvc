58eb3ce7270a9f8ce3cb4a9299faceb8e4b4bc4b
AMAN KUMAR JHA <95953071+Aman4443@users.noreply.github.com>
2026-02-03 18:47:25 +0530

    Authentication Filter

---
+++ Ss/src/main/java/com/revision/Ss/controller/FirstControlller.java
@@ -0,0 +1,24 @@
+package com.revision.Ss.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FirstControlller {
+	@RequestMapping("index")
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+public String index() {
+	return "index";
+}
+	@RequestMapping("/home")
+	@PreAuthorize("hasAuthority('ROLE_USER')")
+	public String home() {
+		return "home";
+	}
+	@RequestMapping("/about")
+	@PreAuthorize("hasAuthority('ROLE_USER')")
+	public String about() {
+		return "about";
+	}
+}
